df60aacc371f6ccb3b25a590299e08bd1a16c306
Esther Gonzalez Peralta <230297@utxicotepec.edu.mx>
2025-02-23 16:30:38 -0600

    Practica1
    Se agregó un boton usando Tailwind y Angular Material

--- ManualEjercicios_Angular_230297/src/app/app.component.ts
+++ ManualEjercicios_Angular_230297/src/app/app.component.ts
@@ -5,10 +5,14 @@ import { Ejercicio02Component } from "./ejercicio-02/ejercicio-02.component";
 import { Ejercicio03Component } from "./ejercicio-03/ejercicio-03.component";
 import { Ejercicio04Component } from "./ejercicio-04/ejercicio-04.component";
 import { Ejercicio05Component } from "./ejercicio-05/ejercicio-05.component";
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Ejercicio01Component, Ejercicio02Component, Ejercicio03Component, Ejercicio04Component, Ejercicio05Component],
+  standalone: true,
+  imports: [RouterOutlet, Ejercicio01Component, Ejercicio02Component, Ejercicio03Component, Ejercicio04Component, Ejercicio05Component, MatButtonModule, MatIconModule, MatTooltipModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
